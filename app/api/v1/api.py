a8a33f0267a0c299627cbfae07c0aff1f18127ca
Noridom1 <thinhphuc2005@gmail.com>
2026-02-04 12:59:08 +0700

    All methods endpoint

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,9 +1,10 @@
 # app/api/v1/api.py
 
 from fastapi import APIRouter
-from app.api.v1.endpoints import aesthetic_ep
+from app.api.v1.endpoints import all_methods_ep
 
 
 api_router = APIRouter()
 
-api_router.include_router(aesthetic_ep.router, prefix="/retrieval", tags=["retrieval"])
+
+api_router.include_router(all_methods_ep.router, prefix="/retrieval", tags=["retrieval"])
