6f44ac3457baa841858f29fd334caa192d6363c2
Noridom1 <thinhphuc2005@gmail.com>
2026-02-05 20:51:17 +0700

    Fix endpoints

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,12 +1,14 @@
 # app/api/v1/api.py
 
 from fastapi import APIRouter
-from app.api.v1.endpoints import all_methods_ep
 from app.api.v1.endpoints import experiment_ep
+from app.api.v1.endpoints import image_edit_ep
+from app.api.v1.endpoints import pe_clip_ep
 
 
 api_router = APIRouter()
 
 
-api_router.include_router(all_methods_ep.router, prefix="/retrieval", tags=["retrieval"])
 api_router.include_router(experiment_ep.router, tags=["study"])
+api_router.include_router(image_edit_ep.router, prefix="/retrieval", tags=["retrieval"])
+api_router.include_router(pe_clip_ep.router, prefix="/retrieval", tags=["retrieval"])
