0a342f305d29695bd2b8193e69a41c931fd8b887
Noridom1 <thinhphuc2005@gmail.com>
2026-02-05 20:46:45 +0700

    Fix endpoints

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,12 +1,13 @@
 # app/api/v1/api.py
 
 from fastapi import APIRouter
-from app.api.v1.endpoints import all_methods_ep
 from app.api.v1.endpoints import experiment_ep
+from app.api.v1.endpoints import aesthetic_ep
 
 
 api_router = APIRouter()
 
 
-api_router.include_router(all_methods_ep.router, prefix="/retrieval", tags=["retrieval"])
+# api_router.include_router(all_methods_ep.router, prefix="/retrieval", tags=["retrieval"])
 api_router.include_router(experiment_ep.router, tags=["study"])
+api_router.include_router(aesthetic_ep.router, prefix="/retrieval", tags=["retrieval"])
