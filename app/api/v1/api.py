876c1032c53e8c42255394b73692bbb27ee7a33d
Noridom1 <thinhphuc2005@gmail.com>
2026-02-05 21:22:02 +0700

    Add VLM endpoint

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -3,7 +3,7 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints import experiment_ep
 from app.api.v1.endpoints import image_edit_ep
-from app.api.v1.endpoints import pe_clip_ep
+from app.api.v1.endpoints import pe_clip_ep, vlm_ep
 
 
 api_router = APIRouter()
@@ -12,3 +12,4 @@ api_router = APIRouter()
 api_router.include_router(experiment_ep.router, tags=["study"])
 api_router.include_router(image_edit_ep.router, prefix="/retrieval", tags=["retrieval"])
 api_router.include_router(pe_clip_ep.router, prefix="/retrieval", tags=["retrieval"])
+api_router.include_router(vlm_ep.router, prefix="/retrieval", tags=["retrieval"])
